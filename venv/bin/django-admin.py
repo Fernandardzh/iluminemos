31daa244c75a51c223cfe1e51428535cbbec5a7f
Fernanda Rodriguez Hernandez <fernandardzh@gmail.com>
2016-04-06 18:59:44 -0500

    fuck tha police

--- venv/bin/django-admin.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-#!/Users/fernandarodriguez/Desktop/iluminemos-5b2c3dc955d31a7a61e144c614b54c487e4e9269/venv/bin/python3
-=======
-#!/Users/fernandarodriguez/Desktop/iluminemosos_de_azul/venv/bin/python3
->>>>>>> ef20a4b5096db7e2600025a10f58864ac03b3516
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
